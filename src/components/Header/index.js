ef57dc254866c13ffec7889eb5131c9c750ccc38
Gavin Asay <gavin.asay@gmail.com>
2021-04-03 01:40:03 -0600

    skeletons for header and nav

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Nav from '../Nav';
+
+function Header() {
+	return (
+		<header>
+			<div>
+				<h1>
+					Gavin Asay
+					<br />
+					Web Developer, MERN Stack
+				</h1>
+				<Nav />
+			</div>
+		</header>
+	);
+}
+
+export default Header;
