b20bbdc95121bd6e8810ebdb5b4c9625cde5e255
Gavin Asay <gavin.asay@gmail.com>
2021-05-02 17:07:40 -0600

    server fix?

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '../client/build')));
+	app.use('/', express.static(path.join(__dirname, '../client/build')));
 }
 
 app.post('/api/sendmessage', async (req, res) => {
