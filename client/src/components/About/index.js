7ad8249e884c013821d2dc6d01db8298facb27f5
Gavin Asay <gavin.asay@gmail.com>
2022-05-10 19:52:13 -0700

    update about text

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -7,20 +7,13 @@ function About() {
 			<div>
 				<img src={portrait} alt="It's me, Gavin!" />
 				<p>Hello! Thank you for visiting.</p>
+				<p>My name is Gavin Asay. I am a full stack web developer.</p>
 				<p>
-					My name is Gavin Asay. I am a MERN stack web developer, coming from the University of Utah coding boot camp.
+					My experience includes full stack development with JavaScript, jQuery and React.js on the front end. On the
+					backend, I have experience with node.js using Express and Sequelize, as well as Python using Flask and
+					SQLAlchemy. Finally, I have a growing proficiency in .NET MVC applications.
 				</p>
-				<p>
-					My interest in web development began in high school, where a class in web design was offered. What started as
-					a glorified Dreamweaver class started to teach HTML after I urged the teacher to let the subject matter expand
-					beyond a single application. I learned CSS on my own at a time when "Tables are for tabular data!" was a
-					slogan among forward-thinking developers.
-				</p>
-				<p>
-					HTML and CSS were skills that opened doors to jobs during and after college. In time, I recognized that many
-					more doors would open if I expanded my skillset beyond the foundations and committed myself to becoming a
-					full-stack developer. Everyone needs a website, and I am here to provide!
-				</p>
+				<p>Please feel free to view projects featured in my portfolio or download my resume.</p>
 			</div>
 		</section>
 	);
